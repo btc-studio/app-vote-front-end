9110ede2e0fe170b4afcf307c9aa15cc4c36ca23
btcs-manhdd <manhdd@btc-studio.io>
2022-06-18 17:00:54 +0700

    init front-end

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import './App.css';
+import React from 'react'
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="text-3xl font-bold text-center ">
-        Vote app
+    <div className="App">
+      <h1 className="text-3xl text-blue-400">App vote Hello</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
