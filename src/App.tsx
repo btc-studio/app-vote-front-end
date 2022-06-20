532c075cc4a4392ee24139f87da51b92c7b25211
Nguyen Duong <muoi07052001@gmail.com>
2022-06-20 10:37:05 +0700

    Fix lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <h1 className="text-3xl text-blue-400">App vote Hello</h1>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
