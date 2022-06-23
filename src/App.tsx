770426cdd2dd14d5bfe1301135424308c77b158a
btcs-manhdd <107126036+btcs-manhdd@users.noreply.github.com>
2022-06-24 00:28:46 +0700

    Base dự án dùng create react app, kết hợp với near (#1)
    * init front-end
    
    * Fix lint
    
    * Init near front-end
    
    * modified .prettierrc
    
    * edit format file
    
    Co-authored-by: Nguyen Duong <muoi07052001@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,36 @@
-import './App.css';
+import React from 'react';
+import { login, logout } from './near/utils';
 
-function App() {
+const App: React.FC = () => {
+  // if not signed in, return early with sign-in prompt
+  if (!window.walletConnection.isSignedIn()) {
+    return (
+      <main>
+        <h1>Voting app</h1>
+        <p style={{ textAlign: 'center', marginTop: '2.5em' }}>
+          <button onClick={login}>Sign in</button>
+        </p>
+      </main>
+    );
+  }
+
   return (
-    <div className="text-3xl font-bold text-center ">
-        Vote app
-    </div>
+    // use React Fragment, <>, to avoid wrapping elements in unnecessary divs
+    <>
+      <button className="link" style={{ float: 'right' }} onClick={logout}>
+        Sign out
+      </button>
+      <main>
+        <h1>{window.accountId}</h1>
+      </main>
+    </>
   );
-}
+
+  // return (
+  //   <div className="App">
+  //     <h1 className="text-3xl text-blue-400">App vote Hello</h1>
+  //   </div>
+  // );
+};
 
 export default App;
